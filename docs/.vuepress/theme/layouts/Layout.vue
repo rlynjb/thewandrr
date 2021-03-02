d2e1a32088fbc6e5bb5f61fc0dc89b7c709081ba
reina lyn ben <reinalynben@reinas-MacBook-Air.local>
2021-03-02 06:52:41 -0800

    init

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app class="theme-container">
+    <Content class="pa-10"/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    // checking for vuetify
+    console.log(this.$options)
+  }
+}
+</script>
+
+<style lang="styl">
+.theme-container a
+  text-decoration: none;
+</style>
