aaeb08876c5323b4372b7a265b41fe8ece9edd50
reinalyn <rlynjb@gmail.com>
2021-03-03 21:17:06 -0800

    wip, homepage

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,6 @@
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import colors from 'vuetify/lib/util/colors';
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -8,5 +9,15 @@ export default ({
   siteData // site metadata
 }) => {
   Vue.use(Vuetify);
-  options.vuetify = new Vuetify({})
+  options.vuetify = new Vuetify({
+    theme: {
+      dark: {
+        primary: colors.purple.lighten3,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+        anchor: colors.purple.lighten3
+      }
+    }
+  })
 };
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,6 +1,47 @@
 <template>
   <v-app class="theme-container">
-    <Content class="pa-10"/>
+    <v-app-bar app class="d-flex">
+      <v-container>
+        <div class="logo">
+          <img src="/logo.png" />
+        </div>
+
+        tutorials
+        vlog
+        resume
+        linkedin
+      </v-container>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <div class="hero pa-5">
+          hero here
+        </div>
+
+        <div class="desc pa-5">
+          desc here
+        </div>
+
+        <div class="skills pa-5">
+          skills here
+        </div>
+
+        <div class="coding pa-5">
+          coding here
+        </div>
+
+        <div class="exploring pa-5">
+          exploring here
+        </div>
+
+        <div class="contact pa-5">
+          contact here
+        </div>
+
+        <Content />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
@@ -21,4 +62,10 @@ export default {
 <style lang="styl">
 .theme-container a
   text-decoration: none;
+
+.theme-container img
+  width: 100%;
+
+.theme-container .logo
+  max-width: 200px;
 </style>
